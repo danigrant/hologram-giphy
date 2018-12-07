d3d536dad80065768be50cecaa89962190ed2c5f
dani <mayor@dani.town>
2018-12-07 10:55:18 -0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+let serverLocation = 'http://localhost:3000'
+
+void async function main() {
+  await getSticker()
+
+}()
+
+async function getSticker() {
+  let res = await window.fetch(`${serverLocation}/sticker`)
+  let json = await res.json()
+  displaySticker(json)
+}
+
+async function displaySticker(stickerObj) {
+  console.log('here');
+  document.getElementById('sticker-north').innerHTML = `<img class="sticker" src="${stickerObj.gifUrl}" />`
+  document.getElementById('sticker-east').innerHTML = `<img class="sticker" src="${stickerObj.gifUrl}" />`
+  document.getElementById('sticker-west').innerHTML = `<img class="sticker" src="${stickerObj.gifUrl}" />`
+  document.getElementById('sticker-south').innerHTML = `<img class="sticker" src="${stickerObj.gifUrl}" />`
+
+}
